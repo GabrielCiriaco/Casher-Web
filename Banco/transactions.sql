d1344260ff4d6598d4744ae91ca76f954322bf02
Gabriel Fornitano <gabrielcfornitano@gmail.com>
2024-09-23 21:33:15 -0300

    Adição do fluxo de transações

--- Banco/transactions.sql
+++ Banco/transactions.sql
@@ -7,7 +7,7 @@ CREATE TABLE transactions (
 	category_id INT,
 	created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE
+	CONSTRAINT FK_user_id FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
 	CONSTRAINT FK_category_id FOREIGN KEY (category_id) REFERENCES categories(id) 
 	
 )
